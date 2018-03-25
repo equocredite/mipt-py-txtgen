98bea6504363310f1abe26e6bba35faf4d810a1e
equocredite <equocredite@gmail.com>
2018-03-25 17:54:33 +0300

    incapsulation

--- generate.py
+++ generate.py
@@ -3,14 +3,27 @@ import sys
 import random
 
 
-# freq[word1] = {word2 : number of pairs {word1, word2} in the corpus}
-model = dict()
+def get_args():
+    """Create a parser, read arguments, return them"""
+    parser = argparse.ArgumentParser(
+        description="Generate a new text on the base of corpus")
+    parser.add_argument("--model",
+                        help="Path to the file with generated model")
+    parser.add_argument("--seed", help="The desired first word; "
+                                       "will be chosen randomly by default")
+    parser.add_argument("--length", help="The desired length of text")
+    parser.add_argument("--output",
+                        help="File in which to save generated text; "
+                             "will be printed to stdout by default")
+    return parser.parse_args()
 
 
-def load_model(instream):
+def load_model(instream, model):
     """Load the model.
     :param instream: pointer to the file with the model
-    :return: None; the model is saved to the dictionary "model"
+    :param model: where to load the model,
+                  dictionary(string : dictionary(string : int))
+    :return: None; the model is saved to "model"
     """
     for line in instream.readlines():
         elems = line.split()
@@ -20,11 +33,12 @@ def load_model(instream):
             model[elems[0]][elems[i]] = int(elems[i + 1])
 
 
-def generate(outstream, seed, length):
+def generate(outstream, seed, length, model):
     """Generate a new text.
     :param outstream: pointer to an output file; stdout if none is specified
     :param seed: the desired first word
     :param length: length of output
+    :param model: same as in documentation for load_model()
     :return: None; the text is saved to the file pointed to by outstream
     """
     while length > 0:
@@ -43,30 +57,30 @@ def generate(outstream, seed, length):
     outstream.write('\n')
 
 
-# creating a parser for parsing command line arguments
-parser = argparse.ArgumentParser(
-    description="Generate a new text on the base of corpus")
-parser.add_argument("--model", help="Path to the file with generated model")
-parser.add_argument("--seed", help="The desired first word; "
-                                   "will be chosen randomly by default")
-parser.add_argument("--length", help="The desired length of text")
-parser.add_argument("--output", help="File in which to save generated text; "
-                                     "will be printed to stdout by default")
-args = parser.parse_args()
+def run():
+    args = get_args()
 
-instream = open(args.model, "r")
-load_model(instream)
-instream.close()
+    # freq[word1] = {word2 : number of pairs {word1, word2} in the corpus}
+    model = dict()
 
-# if the user hasn't specified the first word,
-# then select it randomly from all words in the corpus
-if args.seed is None:
-    args.seed = random.choice(list(model.keys()))
+    instream = open(args.model, "r")
+    load_model(instream, model)
+    instream.close()
 
-# if
-if args.output is None:
-    generate(sys.stdout, args.seed, int(args.length))
-else:
-    outstream = open(args.output, "w")
-    generate(outstream, args.seed, int(args.length))
-    outstream.close()
+    # if the first word is not specified,
+    # then select it randomly from all words in the corpus
+    if args.seed is None:
+        args.seed = random.choice(list(model.keys()))
+
+    # if an output file hasn't been specified,
+    # then print generated text to stdout
+    if args.output is None:
+        generate(sys.stdout, args.seed, int(args.length), model)
+    else:
+        outstream = open(args.output, "w")
+        generate(outstream, args.seed, int(args.length), model)
+        outstream.close()
+
+
+if __name__ == "__main__":
+    run()
--- train.py
+++ train.py
@@ -3,14 +3,12 @@ import sys
 import os
 
 
-# freq[word1] = {word2 : number of pairs {word1, word2} in the corpus}
-freq = dict()
-
-
-def process_file(instream, lc):
+def process_file(instream, lc, model):
     """Read a file, save to freq.
     :param instream: pointer to an input file
     :param lc: True to make all letters lowercase
+    :param model: where to save the model,
+                  dictionary(string : dictionary(string : int))
     """
     # so that we could consider a pair separated by a newline character
     previous_line_end = ""
@@ -27,47 +25,61 @@ def process_file(instream, lc):
                 clean_line += ' '
         words = clean_line.split()
         for i in range(len(words) - 1):
-            if words[i] not in freq:
-                freq[words[i]] = dict()
-            if words[i + 1] not in freq[words[i]]:
-                freq[words[i]][words[i + 1]] = 0
-            freq[words[i]][words[i + 1]] += 1
+            if words[i] not in model:
+                model[words[i]] = dict()
+            if words[i + 1] not in model[words[i]]:
+                model[words[i]][words[i + 1]] = 0
+            model[words[i]][words[i + 1]] += 1
         previous_line_end = words[len(words) - 1]
 
 
-def save_freq(outstream):
+def save_model(outstream, model):
     """Save the model.
     :param outstream: pointer to the file in which to save the model
+    :param model: well, the model itself
     """
-    for i in freq:
+    for i in model:
         outstream.write(i + ' ')
-        for j in freq[i]:
-            outstream.write(j + ' ' + str(freq[i][j]) + ' ')
+        for j in model[i]:
+            outstream.write(j + ' ' + str(model[i][j]) + ' ')
         outstream.write('\n')
 
 
-# creating a parser for parsing command line arguments
-parser = argparse.ArgumentParser(
-    description="Count frequencies of all texts in a corpus and save them")
-parser.add_argument("--input-dir",
-                    help="Path to the folder with some texts to train on")
-parser.add_argument("--model",
-                    help="Path to the file in which the model shall be saved")
-parser.add_argument("--lc", action="store_true",
-                    help="Make all letters lowercase")
+def get_args():
+    """Create a parser, read arguments, return them"""
+    parser = argparse.ArgumentParser(
+        description="Count frequencies of all texts in a corpus and save them")
+    parser.add_argument("--input-dir",
+                        help="Path to the folder with some texts to train on")
+    parser.add_argument("--model",
+                        help="Path to the file"
+                             "in which the model shall be saved")
+    parser.add_argument("--lc", action="store_true",
+                        help="Make all letters lowercase")
+    return parser.parse_args()
 
-args = parser.parse_args()
 
-# if an input directory hasn't been specified, then read the corpus from stdin
-if args.input_dir is None:
-    process_file(sys.stdin, args.lc)
-else:
-    # iterate over all files in the input directory
-    for file_name in os.listdir(args.input_dir):
-        instream = open(args.input_dir + "/" + file_name, "r")
-        process_file(instream, args.lc)
-        instream.close()
+def run():
+    args = get_args()
 
-outstream = open(args.model, "w")
-save_freq(outstream)
-outstream.close()
+    # model[word1] = {word2 : number of pairs {word1, word2} in the corpus}
+    model = dict()
+
+    # if an input directory hasn't been specified,
+    # then read the corpus from stdin
+    if args.input_dir is None:
+        process_file(sys.stdin, args.lc, model)
+    else:
+        # iterate over all files in the input directory
+        for file_name in os.listdir(args.input_dir):
+            instream = open(args.input_dir + "/" + file_name, "r")
+            process_file(instream, args.lc, model)
+            instream.close()
+
+    outstream = open(args.model, "w")
+    save_model(outstream, model)
+    outstream.close()
+
+
+if __name__ == "__main__":
+    run()
